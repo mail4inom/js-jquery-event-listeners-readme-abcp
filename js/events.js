a629c0f7a181d2ce8366f89a5386f04ff9dc869a
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-16 00:44:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,6 +10,12 @@ function frameIt(){
     $("img").addClass("tasty")
   })
 }
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
