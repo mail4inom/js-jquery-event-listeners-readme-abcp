6682d3cf7046e81f456ce03788832c2f97aae903
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-02-16 00:49:53 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,8 +13,8 @@ function frameIt(){
 
 function pressIt(){
   $("input").on("keydown", function(e){
-    if(e.wich == 71){
-      alert("G was pressed!")
+    if(e.which == 71){
+      alert("G was pressed.")
     }
   })
 }
